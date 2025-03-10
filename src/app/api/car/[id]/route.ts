1eaba8b79fdf4b0fd99c5f57aef955cea3f68d97
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2025-03-10 23:16:27 +0100

    Added get requests for the cars, car types, engines and people via id

---
+++ src/app/api/car/[id]/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server';
+import mysql from 'mysql2/promise';
+
+const db = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.NEXT_DB_HOST,
+    port: Number(process.env.NEXT_DB_PORT),
+    user: process.env.NEXT_DB_USER,
+    password: process.env.NEXT_DB_PASSWORD,
+    database: process.env.NEXT_DB_NAME,
+});
+
+export async function GET(request: NextResponse, { params }: { params: Promise<{ id: string }> }) {
+    const id = (await params).id;
+    try {
+        const [rows] = await db.query(`SELECT * FROM car WHERE id=${id}`);
+        return NextResponse.json(rows, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: 'Fehler beim Abrufen der Daten', error }, { status: 500 });
+    }
+}
+
+export async function DELETE(request: NextResponse, { params }: { params: Promise<{ id: string }> }) {
+    const id = (await params).id;
+    try {
+        const [rows] = await db.query(`DELETE FROM car WHERE id=${id}`);
+        return NextResponse.json(rows, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: 'Fehler beim Löschen der Daten', error }, { status: 500 });
+    }
+}
---
+++ src/app/api/person/[id]/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server';
+import mysql from 'mysql2/promise';
+
+const db = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.NEXT_DB_HOST,
+    port: Number(process.env.NEXT_DB_PORT),
+    user: process.env.NEXT_DB_USER,
+    password: process.env.NEXT_DB_PASSWORD,
+    database: process.env.NEXT_DB_NAME,
+});
+
+export async function GET(request: NextResponse, { params }: { params: Promise<{ id: string }> }) {
+    const id = (await params).id;
+    try {
+        const [rows] = await db.query(`SELECT * FROM person WHERE id=${id}`);
+        return NextResponse.json(rows, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: 'Fehler beim Abrufen der Daten', error }, { status: 500 });
+    }
+}
